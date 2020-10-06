2cb590097b40d7f179db5cd471753ea01cc6e9e7
Tejashri Panchbhai <tejashripanchbhai9@gmail.com>
2020-10-06 15:55:22 +0530

    shear to github

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import './game.css';
+
+import Game from './tic-tac-toe/Tic';
+
+const App = () => {
+  return (
+    <>
+      <Game/>
+      
+    </>
+
+  );
+};
+export default App;
