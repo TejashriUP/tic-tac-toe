1fd01e415d6a5bc94968d70d6eb03fd22d1b1e25
Tejashri Panchbhai <tejashripanchbhai9@gmail.com>
2020-10-06 16:16:28 +0530

    shear to git

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+
 import './game.css';
 
 import Game from './Tic';
